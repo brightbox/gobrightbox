c9644f18b1722c5019c135efadad2004831698ac
John Leach <john@johnleach.co.uk>
2015-10-25 21:53:51 +0000

    cloudip map and unmap support

--- cloud_ips.go
+++ cloud_ips.go
@@ -1,5 +1,9 @@
 package brightbox
 
+import (
+	"fmt"
+)
+
 type CloudIP struct {
 	Resource
 	Name            string
@@ -40,6 +44,48 @@ func (c *Client) CloudIP(identifier string) (*CloudIP, error) {
 
 func (c *Client) DestroyCloudIP(identifier string) error {
 	_, err := c.MakeApiRequest("DELETE", "/1.0/cloud_ips/"+identifier, nil, nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// MapCloudIP issues a request to map the cloud ip to the destination. The
+// destination can be an identifier of any resource capable of receiving a Cloud
+// IP, such as a server interface, a load balancer, or a cloud sql instace.
+//
+// To map a Cloud IP to a server, first lookup the server to get it's interface
+// identifier (or use the MapCloudIPtoServer convenience method)
+func (c *Client) MapCloudIP(identifier string, destination string) error {
+	_, err := c.MakeApiRequest("POST", "/1.0/cloud_ips/"+identifier+"/map",
+		map[string]string{"destination": destination}, nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Convenience method to map a Cloud IP to a server. First looks up the server
+// to get the network interface id. Uses the first interface found.
+func (c *Client) MapCloudIPtoServer(identifier string, serverid string) error {
+	server, err := c.Server(serverid)
+	if err != nil {
+		return err
+	}
+	if len(server.Interfaces) == 0 {
+		return fmt.Errorf("Server %s has no interfaces to map cloud ip %s to", server.Id, identifier)
+	}
+	destination := server.Interfaces[0].Id
+	err = c.MapCloudIP(identifier, destination)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// UnMapCloudIP issues a request to unmap the cloud ip.
+func (c *Client) UnMapCloudIP(identifier string) error {
+	_, err := c.MakeApiRequest("POST", "/1.0/cloud_ips/"+identifier+"/unmap", nil, nil)
 	if err != nil {
 		return err
 	}
