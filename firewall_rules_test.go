2ed8147795920cd4d7bff1859bf7eaa307ec1424
John Leach <john@johnleach.co.uk>
2016-02-13 12:33:32 +0000

    Basic TestUpdateFirewallRule

--- firewall_rules_test.go
+++ firewall_rules_test.go
@@ -44,3 +44,44 @@ func TestCreateFirewallRule(t *testing.T) {
 	}
 
 }
+
+func TestUpdateFirewallRule(t *testing.T) {
+	handler := ApiMock{
+		T:            t,
+		ExpectMethod: "PUT",
+		ExpectUrl:    "/1.0/firewall_rules/fwr-k32ls",
+		ExpectBody:   map[string]string{
+			"protocol":"tcp",
+			"source":"grp-xxxxx",
+			"destination":""},
+		GiveBody:     readJson("firewall_rule"),
+	}
+	ts := httptest.NewServer(&handler)
+	defer ts.Close()
+
+	client, err := brightbox.NewClient(ts.URL, "", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	proto := "tcp"
+	dst := ""
+	src := "grp-xxxxx"
+	opts := brightbox.FirewallRuleOptions{
+		Id: "fwr-k32ls",
+		Protocol:    &proto,
+		Source:      &src,
+		Destination: &dst,
+	}
+	p, err := client.UpdateFirewallRule(&opts)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if p == nil {
+		t.Errorf("Didn't return a firewall rule")
+	}
+	if p.Id != "fwr-k32ls" {
+		t.Errorf("firewall rule id is %s", p.Id)
+	}
+
+}
