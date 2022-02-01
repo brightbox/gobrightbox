6068ba603565fbbc65a4693b9056adf5a98499ff
Neil Wilson <neil@aldur.co.uk>
2022-02-01 11:23:17 +0000

    Add Firewall Rule type

---
+++ v2/firewall_rules.go
@@ -0,0 +1,65 @@
+package brightbox
+
+import (
+	"time"
+)
+
+// FirewallRule represents a firewall rule.
+// https://api.gb1.brightbox.com/1.0/#firewall_rule
+type FirewallRule struct {
+	ID              string
+	Source          string         `json:"source"`
+	SourcePort      string         `json:"source_port"`
+	Destination     string         `json:"destination"`
+	DestinationPort string         `json:"destination_port"`
+	Protocol        string         `json:"protocol"`
+	IcmpTypeName    string         `json:"icmp_type_name"`
+	CreatedAt       time.Time      `json:"created_at"`
+	Description     string         `json:"description"`
+	FirewallPolicy  FirewallPolicy `json:"firewall_policy"`
+}
+
+// FirewallRuleOptions is used in conjunction with CreateFirewallRule and
+// UpdateFirewallRule to create and update firewall rules.
+type FirewallRuleOptions struct {
+	ID              string  `json:"-"`
+	FirewallPolicy  *string `json:"firewall_policy,omitempty"`
+	Protocol        *string `json:"protocol,omitempty"`
+	Source          *string `json:"source,omitempty"`
+	SourcePort      *string `json:"source_port,omitempty"`
+	Destination     *string `json:"destination,omitempty"`
+	DestinationPort *string `json:"destination_port,omitempty"`
+	IcmpTypeName    *string `json:"icmp_type_name,omitempty"`
+	Description     *string `json:"description,omitempty"`
+}
+
+// APIPath returns the relative URL path to the collection endpoint
+func (c FirewallRule) APIPath() string {
+	return "firewall_rules"
+}
+
+// PostPath returns the relative URL path to POST an object
+func (c FirewallRule) PostPath(from *FirewallRuleOptions) string {
+	return c.APIPath()
+}
+
+// PutPath returns the relative URL path to PUT an object
+func (c FirewallRule) PutPath(from *FirewallRuleOptions) string {
+	return c.APIPath() + "/" + from.OptionID()
+}
+
+// DestroyPath returns the relative URL path to DESTROY an object
+func (c FirewallRule) DestroyPath(from string) string {
+	return c.APIPath() + "/" + from
+}
+
+// FetchID returns the ID field from the object
+func (c FirewallRule) FetchID() string {
+	return c.ID
+}
+
+// OptionID returns the ID field from and options object
+// ID will be blank for create, and set for update
+func (c FirewallRuleOptions) OptionID() string {
+	return c.ID
+}
---
+++ v2/firewall_rules_test.go
@@ -0,0 +1,64 @@
+package brightbox
+
+import (
+	"testing"
+
+	"gotest.tools/assert"
+)
+
+func TestFirewallRules(t *testing.T) {
+	instance := testAll[FirewallRule](
+		t,
+		"FirewallRule",
+		"firewall_rules",
+		"firewall rule",
+	)
+	assert.Equal(t, instance.ID, "fwr-k32ls")
+}
+
+func TestFirewallRule(t *testing.T) {
+	instance := testInstance[FirewallRule](
+		t,
+		"FirewallRule",
+		"firewall_rules",
+		"firewall_rule",
+		"fwr-k32ls",
+	)
+	assert.Equal(t, instance.Source, "srv-lv426")
+}
+
+func TestCreateFirewallRule(t *testing.T) {
+	newAC := FirewallRuleOptions{}
+	_ = testCreate[FirewallRule](
+		t,
+		"FirewallRule",
+		"firewall_rules",
+		"firewall_rule",
+		"fwr-k32ls",
+		&newAC,
+		"{}",
+	)
+}
+
+func TestUpdateFirewallRule(t *testing.T) {
+	name := "fwp-j3654"
+	uac := FirewallRuleOptions{ID: "fwr-k32ls", FirewallPolicy: &name}
+	_ = testUpdate[FirewallRule](
+		t,
+		"FirewallRule",
+		"firewall_rules",
+		"firewall_rule",
+		"fwr-k32ls",
+		&uac,
+		`{"firewall_policy":"fwp-j3654"}`,
+	)
+}
+
+func TestDestroyFirewallRule(t *testing.T) {
+	testDestroy[FirewallRule](
+		t,
+		"FirewallRule",
+		"firewall_rules",
+		"fwr-k32ls",
+	)
+}
