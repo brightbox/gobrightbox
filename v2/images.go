df39521072c0b92cafe6b2c7e6b4c5aaf184ed33
Neil Wilson <neil@aldur.co.uk>
2022-04-07 10:01:51 +0100

    Add sourcetype and sourcetrigger status to images

--- v2/images.go
+++ v2/images.go
@@ -5,10 +5,14 @@ import (
 
 	"github.com/brightbox/gobrightbox/v2/status/arch"
 	"github.com/brightbox/gobrightbox/v2/status/image"
+	"github.com/brightbox/gobrightbox/v2/status/sourcetrigger"
+	"github.com/brightbox/gobrightbox/v2/status/sourcetype"
 )
 
 //go:generate ./generate_status_enum image creating available deprecated unavailable deleting deleted failed
 //go:generate ./generate_status_enum arch x86_64 i686
+//go:generate ./generate_status_enum sourcetrigger manual schedule
+//go:generate ./generate_status_enum sourcetype upload snapshot
 
 // Image represents a Machine Image
 // https://api.gb1.brightbox.com/1.0/#image
@@ -26,13 +30,13 @@ type Image struct {
 	Official          bool
 	Public            bool
 	Owner             string
-	SourceTrigger     string `json:"source_trigger"`
-	SourceType        string `json:"source_type"`
-	VirtualSize       int    `json:"virtual_size"`
-	DiskSize          int    `json:"disk_size"`
-	MinRAM            *int   `json:"min_ram"`
-	CompatibilityMode bool   `json:"compatibility_mode"`
-	LicenceName       string `json:"licence_name"`
+	SourceTrigger     sourcetrigger.Status `json:"source_trigger"`
+	SourceType        sourcetype.Status    `json:"source_type"`
+	VirtualSize       int                  `json:"virtual_size"`
+	DiskSize          int                  `json:"disk_size"`
+	MinRAM            *int                 `json:"min_ram"`
+	CompatibilityMode bool                 `json:"compatibility_mode"`
+	LicenceName       string               `json:"licence_name"`
 	Ancestor          *Image
 }
 
